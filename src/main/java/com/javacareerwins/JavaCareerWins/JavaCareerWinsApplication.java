e486ebd12645d1a396bd86fafbe6142c42fdd57b
Michael Salvatore <msalvatore22@gmail.com>
2023-01-26 13:49:15 -0500

    First commit

---
+++ src/main/java/com/javacareerwins/JavaCareerWins/JavaCareerWinsApplication.java
@@ -0,0 +1,13 @@
+package com.javacareerwins.JavaCareerWins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCareerWinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCareerWinsApplication.class, args);
+	}
+
+}
